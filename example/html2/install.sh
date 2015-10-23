38af66de749c15d63e6f06cde868b7aac8cb058d
AndrewCRMartin <andrew@bioinf.org.uk>
2015-10-23 15:48:13 +0100

    More on the perl tutorial

--- example/html2/install.sh
+++ example/html2/install.sh
@@ -1,4 +1,8 @@
-dir=${HOME}/public_html/chrootperltutorial
+DEFDIR=${HOME}/public_html/chrootperltutorial
+if [ "X$DIR" == "X" ]
+then
+   DIR=$DEFDIR
+fi
 
 . ../../chrootperl.cfg
 
@@ -19,28 +23,33 @@ then
    exit 1
 fi
 
-if [ ! -d $dir ]
+if [ ! -d $DIR ]
 then
-   mkdir -p $dir
+   mkdir -p $DIR
 fi
 
-if [ ! -d $dir ]
+if [ ! -d $DIR ]
 then
-   echo "Could not create install directory $dir"
+   echo "Could not create install directory $DIR"
    exit 1
 fi
 
-cp -v *.cgi *.html .htaccess $dir
+cp -v *.cgi *.html .htaccess $DIR
 
 # Patch in the path to chrootperl
-perl -e "while(<>) {s/%%DEST%%/\$ENV{'DEST'}/g; print;}"  perl.cgi >$dir/perl.cgi
+perl -e "while(<>) {s/%%DEST%%/\$ENV{'DEST'}/g; print;}"  perl.cgi >$DIR/perl.cgi
 
 cp test.pdb $SANDBOX/tmp
 
 echo " "
 echo " "
-me=`whoami`
-echo "You may now access  the demo code at http://localhost/~${me}/chrootperltutorial/"
+if [ $DIR == $DEFDIR ]
+then
+   me=`whoami`
+   echo "You may now access the demo code at http://localhost/~${me}/chrootperltutorial/"
+else
+   echo "You may now access the demo code at the URL equivalent of $DIR"
+fi
 echo " "
 echo " "
 
