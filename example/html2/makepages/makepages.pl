9f21bc7d8e0bd3b3e3900dda36a7b119d08718ee
AndrewCRMartin <andrew@bioinf.org.uk>
2015-11-10 18:11:22 +0000

    Various improvements

--- example/html2/makepages/makepages.pl
+++ example/html2/makepages/makepages.pl
@@ -1,34 +1,146 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl -s
 
 use strict;
+my $noflags = 1;
 
-my @data = <>;
-
-my $title = GetTitle(@data);
-print "Title: $title\n";
-
-my $style = GetStyle(@data);
-print "Style: $style\n";
-
-my $aMenu  = GetMenuItems(@data);
-print "Menu: |";
-foreach my $menu (@$aMenu)
+if(defined($::h))
 {
-    print " $menu |";
+    UsageDie();
 }
-print "\n";
 
-my $homeMenu = GetHomeMenu(@data);
-print "Home menu item: $homeMenu\n";
-
-my $aPages = GetPages(@data);
-my $pageNum = 0;
-foreach my $aPage (@$aPages)
+if(scalar(@ARGV))
 {
-    my $lastPage = 0;
-    $lastPage = 1 if($pageNum == (scalar(@$aPages) - 1));
-    WritePage($pageNum, $title, $style, $aMenu, $aPage, $lastPage);
-    $pageNum++;
+    WriteCSS();
+
+    my @data = <>;
+
+    my $title = GetTitle(@data);
+    print "Title: $title\n";
+
+    my $style = GetStyle(@data);
+    print "Style: $style\n";
+    
+    my $aMenu  = GetMenuItems(@data);
+    print "Menu: |";
+    foreach my $menu (@$aMenu)
+    {
+        print " $menu |";
+    }
+    print "\n";
+    
+    my $homeMenu = GetHomeMenu(@data);
+    print "Home menu item: $homeMenu\n";
+    
+    my $aPages = GetPages(@data);
+    my $pageNum = 0;
+    foreach my $aPage (@$aPages)
+    {
+        my $lastPage = 0;
+        $lastPage = 1 if($pageNum == (scalar(@$aPages) - 1));
+        WritePage($pageNum, $title, $style, $homeMenu, $aMenu, $aPage, $lastPage);
+        $pageNum++;
+    }
+}
+elsif($noflags)
+{
+    UsageDie();
+}
+
+#******************************************************************
+sub WriteCSS
+{
+    if(open(my $fp, '>', 'theme.css'))
+    {
+        print $fp <<__EOF;
+body {
+  padding-top: 70px;
+  padding-bottom: 30px;
+}
+
+.theme-dropdown .dropdown-menu {
+  display: block;
+  position: static;
+  margin-bottom: 20px;
+}
+
+.theme-showcase > p > .btn {
+  margin: 5px 0;
+__EOF
+        close $fp;
+    }
+
+    if(open(my $fp, '>', 'mycss.css'))
+    {
+        print $fp <<__EOF;
+
+/* Side notes for calling out things
+-------------------------------------------------- */
+ 
+/* Base styles (regardless of theme) */
+.bs-callout {
+margin: 20px 0;
+padding: 15px 30px 15px 15px;
+border-left: 5px solid #eee;
+}
+.bs-callout h4 {
+margin-top: 0;
+}
+.bs-callout p:last-child {
+margin-bottom: 0;
+}
+.bs-callout code,
+.bs-callout .highlight {
+background-color: #fff;
+}
+ 
+/* Themes for different contexts */
+.bs-callout-danger {
+background-color: #fcf2f2;
+border-color: #dFb5b4;
+}
+.bs-callout-warning {
+background-color: #fefbed;
+border-color: #f1e7bc;
+}
+.bs-callout-info {
+background-color: #f0f7fd;
+border-color: #d0e3f0;
+}
+
+.center {
+text-align: center;
+}
+
+.question {
+background-color: #eeeeee;
+border: 1pt solid red;
+border-left: 6pt solid red;
+padding: 2pt 2pt 2pt 6pt;
+border-radius: 0px;
+border-top-right-radius: 5px;
+-moz-border-radius-topright: 5px;
+-webkit-border-top-right-radius: 5px;
+border-bottom-right-radius: 5px;
+-moz-border-radius-bottomright: 5px;
+-webkit-border-bottom-right-radius: 5px;
+}
+
+__EOF
+        close $fp;
+    }
+}
+
+#******************************************************************
+sub UsageDie
+{
+    print <<__EOF;
+
+makepages V1.0 (c) UCL, Dr. Andrew C.R. Martin
+
+Usage: makepages [-bootstrap] [file.html]
+       makepages file.html
+
+__EOF
 }
 
 #******************************************************************
@@ -116,10 +228,10 @@ sub GetPages
 }
 
 #******************************************************************
-#>     WritePage($pageNum, $title, $style, $aMenu, $aPage, $lastPage);
+#>     WritePage($pageNum, $title, $style, $homeMenu, $aMenu, $aPage, $lastPage);
 sub WritePage
 {
-    my ($pageNum, $title, $style, $aMenu, $aPage, $lastPage) = @_;
+    my ($pageNum, $title, $style, $homeMenu, $aMenu, $aPage, $lastPage) = @_;
 
     my $filename = 'index.html';
     if($pageNum)
@@ -183,16 +295,39 @@ sub PrintHTMLPage
 {
     my($fp, $aPage) = @_;
 
-    print $fp <<__EOF;
- <div class="container theme-showcase">
-__EOF
+    print $fp " <div class='container theme-showcase'>\n";
+
+    FixUp_bigheading($aPage);
 
     foreach my $line (@$aPage)
     {
         print $fp $line;
     }
 
+    print $fp " </div> <!-- /container -->\n";
 }
+
+#******************************************************************
+sub FixUp_bigheading
+{
+    my($aPage) = @_;
+
+    foreach my $line (@$aPage)
+    {
+        $line = Replace($line, 'bigheading','<div class="jumbotron">');
+        $line = Replace($line, '/bigheading','</div> <!-- jumbotron -->');
+    }
+}
+
+#******************************************************************
+sub Replace
+{
+    my($line, $old, $new) = @_;
+    my $regex = '<!--\s+\[' . $old . '\]\s+--\>';
+    $line =~ s/$regex/$new/;
+    return($line);
+}
+
 
 #******************************************************************
 #> PrintHTMLFooter($fp);
@@ -202,16 +337,18 @@ sub PrintHTMLFooter
 
     print $fp <<__EOF;
 
- </div> <!-- /container -->
-
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+<!--
     <script src="bootstrap/assets/js/jquery.js"></script>
     <script src="bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="bootstrap/assets/js/holder.js"></script>
     <script src="autotooltip.js"></script>
-
+-->
   </body>
 </html>
 __EOF
@@ -266,8 +403,8 @@ __EOF
         if($i)
         {
             $filename = sprintf("page%02d.html", $i);
-            print $fp "<li$active><a href='$filename'>$$aMenu[$i]</a></li>\n";
         }
+        print $fp "<li$active><a href='$filename'>$$aMenu[$i]</a></li>\n";
     }
 
     print $fp <<__EOF;
@@ -301,28 +438,33 @@ sub PrintHTMLHeader
     <meta name="author" content="">
     <link rel="shortcut icon" href="bootstrap/dist/ico/favicon.png">
 
-    <title>$title</title>
-
     <!-- Bootstrap core CSS -->
-    <link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" crossorigin="anonymous">
+
     <!-- Bootstrap theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" crossorigin="anonymous">
+
+<!-- 
+    <link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet">
     <link href="bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+-->
 
     <!-- Custom styles for this template -->
     <link href="theme.css" rel="stylesheet">
 
     <!-- And my own useful extras -->
-<!--    <link href="mycss.css" rel="stylesheet"> 
--->
+    <link href="mycss.css" rel="stylesheet"> 
 
     <!-- Any style information from the web page -->
     $style
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <title>$title</title>
   </head>
 
   <body>
