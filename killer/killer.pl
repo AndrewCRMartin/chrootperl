838f1288ab2b93e53e0717c93691247c38f33b4b
Andrew Martin <Andrew C.R. Martin>
2017-01-18 11:13:56 +0000

    Completed killer script

--- killer/killer.pl
+++ killer/killer.pl
@@ -1,30 +1,100 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -s
+#*************************************************************************
+#
+#   Program:    killer
+#   File:       killer.pl
+#   
+#   Version:    V1.0
+#   Date:       18.01.17
+#   Function:   Kills long running jobs according to patterns specified
+#               at the top of the code
+#   
+#   Copyright:  (c) Dr. Andrew C. R. Martin, UCL, 2017
+#   Author:     Dr. Andrew C. R. Martin
+#   Address:    Institute of Structural and Molecular Biology
+#               Division of Biosciences
+#               University College
+#               Gower Street
+#               London
+#               WC1E 6BT
+#   EMail:      andrew@bioinf.org.uk
+#               
+#*************************************************************************
+#
+#   This program is not in the public domain, but it may be copied
+#   according to the conditions laid out in the accompanying file
+#   COPYING.DOC
+#
+#   The code may be modified as required, but any modifications must be
+#   documented so that the person responsible can be identified. If 
+#   someone else breaks this code, I don't want to be blamed for code 
+#   that does not work! 
+#
+#   The code may not be sold commercially or included as part of a 
+#   commercial product except as described in the file COPYING.DOC.
+#
+#*************************************************************************
+#
+#   Description:
+#   ============
+#   Designed for use with the example Perl tutorial, this program sits
+#   and kills jobs that have been running for longer than a specified
+#   time.
+#
+#*************************************************************************
+#
+#   Usage:
+#   ======
+#   ./killer.pl (probably as root)
+#
+#*************************************************************************
+#
+#   Revision History:
+#   =================
+#   V1.0   18.01.17   Original
+#
+#*************************************************************************
 use strict;
+#*************************************************************************
+# CONFIGURATION
+# ------------
+# User with jobs to kill
+my $user      = (defined($::u))?$::u:'apache';
+#my $user      = 'localuser';
 
-%::jobs = ();
+# Executables to kill
+my @jobnames  = ('perl.*/var/tmp.*/tmp/test.pdb'    , 'perl.cgi');
+#my @jobnames  = ('perl.*hello.*sleep', 'perl.cgi');
 
-my $user      = 'localuser';
-my @jobnames  = ('perl'    , 'perl.cgi');
-#my @params    = ('/var/tmp', '');
-my @params    = ('hello', '');
+# Time to allow a job to run
 my $timeLimit = 10;
 
-while(<>)
+#*************************************************************************
+%::jobs = ();
+
+#*************************************************************************
+while(1)
 {
-    KillLongJobs($user, \@jobnames, \@params, $timeLimit);
-    sleep 2;
+    KillLongJobs($user, \@jobnames, $timeLimit);
+    sleep 5;
 }
 
+#*************************************************************************
 sub KillLongJobs
 {
-    my($user, $aJobnames, $aParams, $timeLimit) = @_;
+    my($user, $aJobnames, $timeLimit) = @_;
 
-    my $jobData = GetJobData($user, $aJobnames, $aParams);
-    print $jobData;
+    my $jobData = GetJobData($user, $aJobnames);
+    if(defined($::d) && ($jobData ne ''))
+    {
+        print "Found jobs:\n";
+        print "$jobData\n";
+    }
 
     _killTheLongOnes($jobData, $timeLimit);
 }
 
+#*************************************************************************
 sub _killTheLongOnes
 {
     my ($jobData, $timeLimit) = @_;
@@ -38,7 +108,12 @@ sub _killTheLongOnes
         {
             if((time() - $::jobs{$jobID}) > $timeLimit)
             {
-                print "KILL $jobID\n";
+                my $exe = "kill -9 $jobID";
+                if(defined($::d))
+                {
+                    print "Killing job: $jobID\n";
+                }
+                `$exe`;
                 delete $::jobs{$jobID};
             }
         }
@@ -49,20 +124,16 @@ sub _killTheLongOnes
     }
 }
 
+#*************************************************************************
 sub GetJobData
 {
-    my($user, $aJobnames, $aParams) = @_;
+    my($user, $aJobnames) = @_;
     my $data = '';
     for(my $jobCount=0; $jobCount<scalar(@$aJobnames); $jobCount++)
     {
         my $jobname  = $$aJobnames[$jobCount];
-        my $jobparam = $$aParams[$jobCount];
 
         my $grepString = "grep $jobname";
-        if($jobparam ne '')
-        {
-            $grepString .= " | grep $jobparam";
-        }
 
         my $result = `ps -U $user -u $user www | $grepString | grep -v sh | grep -v grep`;
         $data .= $result;
